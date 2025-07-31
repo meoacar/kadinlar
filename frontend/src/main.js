12c369e121f0eb082f05d5bf550fc79e32f134d3
MeoACAR <meofeat@gmail.com>
2025-07-31 23:53:33 +0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import './assets/app.css'
 
 // Axios global config
-axios.defaults.baseURL = 'http://localhost:8001/api'
+axios.defaults.baseURL = 'http://localhost:8000/api'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
 const app = createApp(App)
