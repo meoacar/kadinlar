657e3347dd35460d72eae53f7566e083a1d2a23e
MeoACAR <meofeat@gmail.com>
2025-07-31 20:48:55 +0300

    Update User.php

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,27 +6,41 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'birth_date',
+        'birth_time',
+        'birth_place',
+        'zodiac_sign_id',
+        'phone',
+        'bio',
+        'avatar',
+        'instagram',
+        'twitter',
+        'facebook',
+        'role',
+        'subscription_type',
+        'subscription_expires_at',
+        'email_verified',
+        'is_active',
+        'last_login_at',
+        'privacy_settings',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -35,14 +49,38 @@ class User extends Authenticatable
 
     /**
      * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
      */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'birth_date' => 'date',
+            'birth_time' => 'datetime',
+            'subscription_expires_at' => 'datetime',
+            'last_login_at' => 'datetime',
+            'email_verified' => 'boolean',
+            'is_active' => 'boolean',
+            'privacy_settings' => 'array',
             'password' => 'hashed',
         ];
+    }
+
+    // Relations
+    public function zodiacSign()
+    {
+        return $this->belongsTo(ZodiacSign::class);
+    }
+
+    // Accessors
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getIsSubscribedAttribute()
+    {
+        return $this->subscription_type !== 'free' && 
+               $this->subscription_expires_at && 
+               $this->subscription_expires_at->isFuture();
     }
 }
