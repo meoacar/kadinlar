e6c9ee2fc7f37d146d2c330ed695b68c64865d96
MeoACAR <meofeat@gmail.com>
2025-07-31 22:19:24 +0300

    q

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import './assets/app.css'
 
 // Axios global config
 axios.defaults.baseURL = 'http://localhost:8001/api'
